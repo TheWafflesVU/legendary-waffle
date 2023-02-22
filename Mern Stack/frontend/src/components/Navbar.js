2c03e3e8544c51e9202883138a33bb1e5ce4810f
siyao-zhu <siyao.zhu@vanderbilt.edu>
2023-02-22 14:37:19 -0600

    small change

--- Mern Stack/frontend/src/components/Navbar.js
+++ Mern Stack/frontend/src/components/Navbar.js
@@ -14,7 +14,7 @@ const Navbar = () => {
     <header>
       <div className="container">
         <Link to="/">
-          <h1>Workout Buddy</h1>
+          <h1>Legendary Waffle</h1>
         </Link>
         <nav>
           {user && (
