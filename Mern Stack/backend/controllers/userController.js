9052c98e660e2b174bb132982d76d677f17ed38d
siyao-zhu <siyao.zhu@vanderbilt.edu>
2023-02-28 16:46:55 -0600

    2/28 16:46

--- Mern Stack/backend/controllers/userController.js
+++ Mern Stack/backend/controllers/userController.js
@@ -23,7 +23,7 @@ const loginUser = async (req, res) => {
 
 // signup a user
 const signupUser = async (req, res) => {
-  // const {email, password} = req.body
+  const {email, password} = req.body
 
   try {
     const user = await User.signup(email, password)
