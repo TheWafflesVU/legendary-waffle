67b5945c385c3e73af9f8d4184b03646eeb4f893
Junhao-Hui <junhao.hui@vanderbilt.edu>
2023-03-07 04:28:00 -0600

    Fully Functional Swipe Card system by Junhao

--- Mern Stack/frontend/src/components/searchbar.js
+++ Mern Stack/frontend/src/components/searchbar.js
@@ -1,41 +1,3 @@
-// import styles from "./search.css";
-// import { useState } from 'react';
-
-// const Search = ({ }) => {
-//   const [search, setSearch] = useState('');
-
-//   const handleSubmit = async (e) => {
-//     e.preventDefault();
-//     // Navigate to projectsearchres.js page
-//     window.location.href = `/projectsearchres?search=${search}`;
-//   }
-
-//   return (
-//     <form className="create" onSubmit={handleSubmit}>
-//       <div className="search-container">
-//         <label htmlFor="search-input">Search Projects</label>
-//         <div className="icon">
-//           <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="#657789" strokeWidth="3" strokeLinecap="round" strokeLinejoin="round" className="feather feather-search">
-//             <circle cx="11" cy="11" r="8"></circle>
-//             <line x1="21" y1="21" x2="16.65" y2="16.65"></line>
-//           </svg>
-//         </div>
-//         <input
-//           id="search-input"
-//           type="text"
-//           className="search"
-//           placeholder="Search"
-//           value={search}
-//           onChange={({ currentTarget: input }) => setSearch(input.value)}
-//         />
-//         <button type="submit">Search</button>
-//       </div>
-//     </form>
-//   );
-// };
-
-// export default Search;
-
 import styles from "./search.css";
 import { useState } from 'react';
 
@@ -45,7 +7,9 @@ const Search = ({ }) => {
     const handleSubmit = async (e) => {
         e.preventDefault()
         // Navigate to projectsearchres.js page
+        console.log(search);
         window.location.href = `/projectsearchres?search=${search}`;
+        
     }
 
     return (
