51079ab37c164dfcce9a3135ace3f8a4a22b5a6a
Helen Sun <xutong.sun@vanderbilt.edu>
2023-04-06 11:07:38 -0500

    email confirmation

--- Mern Stack/backend/models/userModel.js
+++ Mern Stack/backend/models/userModel.js
@@ -1,6 +1,16 @@
+const nodemailer = require('nodemailer');
+const jwt = require('jsonwebtoken');
 const mongoose = require('mongoose')
 const bcrypt = require('bcrypt')
 const validator = require('validator')
+
+const transporter = nodemailer.createTransport({
+  service: 'Gmail',
+  auth: {
+    user: "vu.legendary.waffle@gmail.com",
+    pass: "ABCabc123!",
+  },
+});
 // const { ObjectId } = require('mongodb')
 
 const Schema = mongoose.Schema
@@ -15,6 +25,10 @@ const userSchema = new Schema({
     type: String,
     required: true
   },
+  confirmed: {
+    type: Boolean,
+    defaultValue: false
+  }
   
 })
 
@@ -43,8 +57,28 @@ userSchema.statics.signup = async function(email, password) {
 
   const salt = await bcrypt.genSalt(10)
   const hash = await bcrypt.hash(password, salt)
-
   const user = await this.create({ email, password: hash })
+   // async email
+   jwt.sign(
+    {
+      user: user.id,
+    },
+    process.env.SECRET, // ask
+    {
+      expiresIn: '1d',
+    },
+    (err) => {
+      const url = `http://localhost:3000/confirmation/${ process.env.SECRET }`; // ask
+
+      transporter.sendMail({
+        to: email,
+        subject: 'Confirm Email',
+        html: `Please click this email to confirm your email: <a href="${url}">${url}</a>`,
+      });
+    },
+  );
+
+  
 
   return user
 }
@@ -59,6 +93,10 @@ userSchema.statics.login = async function(email, password) {
   const user = await this.findOne({ email })
   if (!user) {
     throw Error('Incorrect email')
+  }
+
+  if (!user.confirmed) {
+    throw Error('Please confirm your email to login')
   }
 
   const match = await bcrypt.compare(password, user.password)
