e2698693d90a2ee47c7a8fa7bf391b96bfdb1af4
Junhao-Hui <junhao.hui@vanderbilt.edu>
2023-04-14 03:42:07 -0500

    minor

--- Mern Stack/backend/routes/user.js
+++ Mern Stack/backend/routes/user.js
@@ -2,11 +2,7 @@ const express = require('express')
 const auth = require('../middleware/requireAuth')
 
 // controller functions
-<<<<<<< HEAD
-const { loginUser, signupUser, updateUser, deleteUser, getUser } = require('../controllers/userController')
-=======
-const { loginUser, signupUser, updateUser, deleteUser, joinRoom, getRoomNumber } = require('../controllers/userController')
->>>>>>> 349291ac (Chatbox fully functional)
+const { loginUser, signupUser, updateUser, deleteUser, joinRoom, getRoomNumber, getUser } = require('../controllers/userController')
 
 const router = express.Router()
 
@@ -22,15 +18,13 @@ router.put('/:id', auth, updateUser)
 // delete route
 router.delete('/:email', auth, deleteUser)
 
-<<<<<<< HEAD
-// GET a user profile
-router.get('/:id', auth, getUser)
-=======
 // join room route
 router.patch('/join_room', auth, joinRoom)
 
 // Get all room number routes for a user
 router.get('/room_num', auth, getRoomNumber)
->>>>>>> 349291ac (Chatbox fully functional)
+
+// GET a user profile
+router.get('/:id', auth, getUser)
 
 module.exports = router
