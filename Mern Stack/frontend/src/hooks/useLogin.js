f1c8f8b2cf72cbd4e0042415b4d0722dbeafb5e3
Junhao-Hui <junhao.hui@vanderbilt.edu>
2023-04-20 21:52:00 -0500

    tmp

--- Mern Stack/frontend/src/hooks/useLogin.js
+++ Mern Stack/frontend/src/hooks/useLogin.js
@@ -10,7 +10,7 @@ export const useLogin = () => {
     setIsLoading(true)
     setError(null)
 
-    const response = await fetch(`${process.env.API_BASE}/api/user/login`, {
+    const response = await fetch(`${API_BASE}/api/user/login`, {
       method: 'POST',
       headers: {'Content-Type': 'application/json'},
       body: JSON.stringify({ email, password })
