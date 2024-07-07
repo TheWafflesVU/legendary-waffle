e30c75b82c0a43ab187673a613fed7ace819c884
Junhao-Hui <junhao.hui@vanderbilt.edu>
2024-07-07 22:20:37 +0800

    Usersnapshot optimized

--- Mern Stack/frontend/src/pages/components/project card/UserSnapshot.js
+++ Mern Stack/frontend/src/pages/components/project card/UserSnapshot.js
@@ -5,21 +5,31 @@ import './UserSnapShot.css'
 const UserSnapshot = ({userId}) => {
 
     const [userProfile, setUserProfile] = useState({})
+    const [loaded, setLoaded] = useState(false)
     const { user } = useAuthContext();
 
     useEffect(() => {
-        fetch(`/api/user/${userId}`, {
-            headers: {
-                "Content-Type": "application/json",
-                Authorization: `Bearer ${user.token}`,
-            }
-        })
-        .then(res => res.json())
-        .then(data => setUserProfile(data))
+
+        if (user && userId) {
+            fetch(`/api/user/${userId}`, {
+                headers: {
+                    "Content-Type": "application/json",
+                    Authorization: `Bearer ${user.token}`,
+                }
+            })
+                .then(res => res.json())
+                .then(data => {
+                    setUserProfile(data)
+                    setLoaded(true)
+                })
+        }
 
     }, [userId])
 
 
+    if (!loaded) {
+        return <div>Loading...</div>;
+    }
     return (
         <div className="userSnapshot">
             <p className="userName">{userProfile.firstName + " " + userProfile.lastName}</p>
